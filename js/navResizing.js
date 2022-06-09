b79731c16e86135d184a2656fca11ba4eef1b857
Leonel Alegre <leonelalegre.dev@gmail.com>
2022-06-09 17:09:27 -0300

    fixing nav alignment

--- js/navResizing.js
+++ js/navResizing.js
@@ -1,7 +1,3 @@
 const canvasContainer = document.getElementById('canvasContainer');
 
 canvasContainer.style.height = window.innerHeight + "px";
-
-window.addEventListener('resize', function(){
-    canvasContainer.style.height = window.innerHeight + "px";
-});
